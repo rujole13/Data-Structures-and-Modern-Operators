f7d1e6e44df7324ddeb4bc880aae3f60804729e3
Russell Lee <sjkrlee50@gmail.com>
2023-06-06 22:57:46 -0400

    more string methods

--- script.js
+++ script.js
@@ -53,6 +53,25 @@ const restaurant = {
   },
 };
 
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+console.log(flights.split('+'));
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''} ${type
+    .replaceAll('_', ' ')
+    .trim()} from ${getCode(from)} to ${getCode(to)} (${time.replace(
+    ':',
+    'h'
+  )})`.padStart(44);
+  console.log(output);
+}
+
+/*
 const maskCreditCard = function (number) {
   const str = String(number);
   const last = str.slice(-4);
@@ -75,7 +94,6 @@ planesInLine(5);
 planesInLine(3);
 planesInLine(12);
 
-/*
 console.log('a+very+nice+string'.split('+'));
 console.log('Russell Lee'.split(' '));
 
